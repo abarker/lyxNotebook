8d2d82cc5a1cdde13ebb586b8a82ecb697b85e21
Allen Barker <Allen.L.Barker@gmail.com>
2020-03-18 09:14:34 -0400

    change to pair of double right-angle for magic cookie

--- src/lyxNotebook/lyxNotebook_user_settings.py
+++ src/lyxNotebook/lyxNotebook_user_settings.py
@@ -64,7 +64,9 @@ num_backup_buffer_copies = 5
 # The cookie string which is temporarily inserted.  Only alphanumeric
 # strings have been tested.  Definitely cannot contain a semicolon.
 magic_cookie_string = "zZ3Qq"
-magic_cookie_string = "»" # Right-pointing double angle quotation mark
+magic_cookie_string = "œ" # Ligature OE (Latin).
+magic_cookie_string = "»" # Right-pointing double angle quotation mark.
+magic_cookie_string = "»»" # Right-pointing double angle quotation mark.
 
 # Whether Lyx Notebook should start up separate interpreter processes for the same
 # cell types in different buffers.
